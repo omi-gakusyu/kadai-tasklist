5cdc27ba051f854d879847a9be62d0888c34d11c
omi.gakusyu <rubygakusyuu@gmail.com>
2020-01-28 15:10:34 +0000

    add colum

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
   private
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
   def set_task
